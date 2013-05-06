e7859689d5ba5a0402233ae7415b47120578af62
maineparnisari <maineparnisari@d6609454-101f-0e72-e24d-771d2626999a>
2013-05-06 01:39:42 +0000

    Subo lo de hashing extensible de Nadia.

--- trunk/src/CapaLogica/HashingExtensible/HashingExtensible.h
+++ trunk/src/CapaLogica/HashingExtensible/HashingExtensible.h
@@ -4,18 +4,22 @@
 #include "Tabla.h"
 #include "../../CapaFisica/RegistroVariable.h"
 #include "../../Constantes.h"
+#include "../../CapaFisica/Bloque.h"
+#include "../../CapaFisica/ManejadorBloques.h"
+#include "../../CapaLogica/ManejoArchivos/ClaveX.h"
+#include "../../CapaLogica/ManejoArchivos/Clave.h"
+#include "../../CapaLogica/ManejoArchivos/RegistroClave.h"
+
 
 class HashingExtensible
 {
-    /*Atributos.....
+    /*Atributos..... No se necesitan guardar en el archivo pero son utiles en RAM*/
     private:
 
-        string fileName;
-        Tabla tabla;
-        Bloques bloques;
+        std::string fileName;
         int cant_bloques;
-        lista bloques_libres;   //manejo de bloques libres
-    */
+        ManejadorBloques manejador_bloques;
+
 
 	public:
 
@@ -26,25 +30,44 @@ class HashingExtensible
 
 		virtual ~HashingExtensible();
 
-		virtual int agregar(RegistroVariable& reg);
+		virtual int agregar(RegistroClave reg);
 		//Guarda el elemento en el hash, en caso de que ya existe el elemento devuelve YA_EXISTE
 
 		virtual int create(std::string directorioSalida, std::string fileName);
 		//NO SE PARA QUE LO PUSIERON*************************
 
-        virtual RegistroVariable devolver(int clave);
+        virtual int devolver(ClaveX clave, RegistroClave *reg);
         //Devuelve el elemento del hash, en caso de que no exista devuelve NO_EXISTE
 
-        virtual int modificar(int clave, RegistroVariable elemN);
+        virtual int modificar(RegistroClave elemN);
         //Modifica el elemento buscado, en caso de no encontrarlo devuelve NO_EXISTE
 
-        virtual int eliminar(int clave);
+        virtual int eliminar(ClaveX clave);
         //Elimina el elemento
 
     private:
 
         virtual int funcion_dispersion(int clave);
+        //Devuelve el numero de bloque donde se debe guardar esa clave
 
+        virtual int crear_bloque(int tam, Bloque *bloqueNuevo);
+        //Crea un nuevo bloque del Hashing
+
+        virtual int guardar_tabla(Tabla *tabla);
+        //Guarda la tabla en el archivo del Hashing
+
+        virtual char* obtener_tabla();
+        //virtual int obtener_tabla(Tabla *tabla);
+        //Nos devuelve la tabla que esta guardada en el archivo del Hashing
+
+        virtual int obtener_bloque(ClaveX clave, Bloque *bloque);
+        //Obtenemos el bloque que corresponde a la clave pasada por parametro
+
+        virtual int obtener_posicion_reg_bloque(ClaveX clave, Bloque bloque);
+        //Obtenemos en que posicion se encuentra el registro con la clave que buscamos
+        //dentro de un bloque
+
+        virtual int obtener_posicion_bloque(ClaveX elemN);
 };
 
 #endif /* HASHINGEXTENSIBLE_H */
