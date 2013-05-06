50e167f92e62e71221a2c50da0e5c77baaf75c85
maineparnisari <maineparnisari@d6609454-101f-0e72-e24d-771d2626999a>
2013-05-06 04:56:04 +0000

    Agregue un metodo truncar en la clase ManejadorDatosBasicos. Sirve para la tabla de hash.

--- trunk/src/CapaFisica/ManejadorDatosBasicos.h
+++ trunk/src/CapaFisica/ManejadorDatosBasicos.h
@@ -3,29 +3,36 @@
 
 #include "ManejadorArchivos.h"
 
-template<typename TipoDato>
+struct mdb_header
+{
+	unsigned int tamanio_archivo;
+};
 
+template<typename TipoDato>
 class ManejadorDatosBasicos: public ManejadorArchivos {
 
 	public:
 		ManejadorDatosBasicos();
 		virtual ~ManejadorDatosBasicos();
 
+		int truncar(long longitudDeseada);
 		int agregar(const TipoDato dato);
 		int escribir(const TipoDato dato, int pos);
-		int leer(TipoDato dato, int pos);
+		int leer(TipoDato* dato, int pos);
 
-		int crear_archivo(std::string nombreArchivo, unsigned int tamBloque);
+		int crear_archivo(std::string nombreArchivo);
 		int eliminar_archivo(std::string nombreArchivo);
 
 		int abrir_archivo(std::string nombreArchivo, std::string modo);
 		int cerrar_archivo();
 
 	private:
+		struct mdb_header header;
 		FILE* file_handler;
 		std::string nombreArchivo;
-
-		int get_tamanio_archivo();
+		int __get_header();
+		int __set_header();
+		int __get_tamanio_archivo();
 };
 
 #endif /* MANEJADORDATOSBASICOS_H_ */
