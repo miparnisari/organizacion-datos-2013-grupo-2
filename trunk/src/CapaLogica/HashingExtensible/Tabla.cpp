92d9576d75c01dbc25e438d4318afc9182b1d50a
nani16.galli@gmail.com <nani16.galli@gmail.com@d6609454-101f-0e72-e24d-771d2626999a>
2013-05-06 02:15:39 +0000

    Subo por el momento un par de modificaciones en esta clase para que compile el Hashing, despues seran modificados con los nombres que correspondan

--- trunk/src/CapaLogica/HashingExtensible/Tabla.cpp
+++ trunk/src/CapaLogica/HashingExtensible/Tabla.cpp
@@ -1,1 +1,21 @@
 #include "../HashingExtensible/Tabla.h"
+
+        Tabla::Tabla(){}
+
+        Tabla::Tabla(char* c_tabla){}
+
+        Tabla::~Tabla(){}
+
+        int Tabla::modificar_pos(int pos, int dato){}
+
+        int Tabla::tam(){}
+
+        int Tabla::obtener_valor(int pos){}
+
+        char* Tabla::devolver_string(){}
+
+        int Tabla::mover_der(int cantPos){}
+
+        int Tabla::mover_izq(int cantPos){}
+
+        int Tabla::duplicar_tabla(){}
--- trunk/src/CapaLogica/HashingExtensible/Tabla.h
+++ trunk/src/CapaLogica/HashingExtensible/Tabla.h
@@ -1,12 +1,33 @@
 #ifndef TABLA_H
 #define TABLA_H
 
+
 class Tabla
 {
 	private:
 		/* add your declarations */
-	
+
 	public:
+
+        Tabla();
+
+        Tabla(char *c_tabla);
+
+        ~Tabla();
+
+        virtual int modificar_pos(int pos, int dato);
+
+        virtual int tam();
+
+        virtual int obtener_valor(int pos);
+
+        virtual char *devolver_string();
+
+        virtual int mover_der(int cantPos);
+
+        virtual int mover_izq(int cantPos);
+
+        virtual int duplicar_tabla();
 		/* add your declarations */
 
 };
