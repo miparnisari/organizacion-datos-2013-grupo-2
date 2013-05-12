de1d49725beaeabbf91900b8a74d4bfb76159e6e
niko.sibi@gmail.com <niko.sibi@gmail.com@d6609454-101f-0e72-e24d-771d2626999a>
2013-05-12 17:59:23 +0000

    modifique la tabla como queria maine, asi todo se trabaja por medio de un archivo, en un rato termino de subir tests. se eliminaron las clases listade y nodo

--- src/CapaLogica/HashingExtensible/Tabla.h
+++ src/CapaLogica/HashingExtensible/Tabla.h
@@ -9,7 +9,7 @@
 #define TABLA_H_
 #include <iostream>
 #include <cstddef>
-#include "ListaDE.h"
+#include "../../Constantes.h"
 #include "../../CapaFisica/ManejadorArchivoDatosBasicos.h"
 
 
@@ -19,27 +19,19 @@ using namespace std;
 class Tabla {
 private:
 	int tamanio;
-	ListaDE<int>* bloques;
 
 public:
-	Tabla();
-	Tabla(string tablaPersistida);
+	Tabla(string rutaArchivo);
 	virtual ~Tabla();
-
-	ListaDE<int>* getBloques();
 
 	void set_tamanio(int cantidad);
 	int get_tamanio();
 
-    string persistir_tabla(); //Esta me dijo nico que iba....
-	int obtener_valor(int posicion);
-	void cambiar_valor(int posicion, int nuevoValor);
-	void dividir_tabla();
-	void duplicar_tabla();
-
-	void persistir_tabla(string rutaArchivo);
-	void cargar_tabla(string rutaArchivo);
-
+	int obtener_valor(string rutaArchivo, int posicion);
+	void cambiar_valor(string rutaArchivo, int posicion, int nuevoValor);
+	void dividir_tabla(string rutaArchivo);
+	void duplicar_tabla(string rutaArchivo);
+	void eliminar_tabla(string rutaArchivo);
 };
 
 #endif /* TABLA_H_ */
