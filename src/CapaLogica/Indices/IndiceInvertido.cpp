4b723adacb8f28d9772c441f2f737fa6f9e87b6d
nani16.galli@gmail.com <nani16.galli@gmail.com@d6609454-101f-0e72-e24d-771d2626999a>
2013-05-17 07:47:47 +0000

    Subo mas indices, modifique la gran mayoria y seguire asi hasta que caiga vencida....

--- src/CapaLogica/Indices/IndiceInvertido.cpp
+++ src/CapaLogica/Indices/IndiceInvertido.cpp
@@ -32,6 +32,27 @@ int IndiceInvertido::agregar_documento(std::string doc, int IDdoc)
     this->archivo_coincidencias.eliminar_archivo(this->fileName+"Coincidencias");
 }
 
+int IndiceInvertido::agregar_termino(char* termino, int IDdoc, int pos)
+{
+    int posNodoSecuencial, IDter;
+    char *idtermino;
+    RegistroClave regClave;
+    ClaveX clave;
+    clave.set_clave(termino);
+    regClave.set_clave(clave);
+    //Vemos si el termino se encuentra en el vocabulario
+    posNodoSecuencial = this->vocabulario.buscar(regClave);
+    if (posNodoSecuencial== RES_ERROR){ /******Si no existe en el buscar del arbol*/
+        IDter = this->archivo_terminos.get_cantidad_registros_ocupados();
+        idtermino = itoa (IDter, idtermino, 10); /*****chanchada para guardar numero como char***/
+        this->crear_termino_vocabulario(clave, idtermino);
+    }else{
+        /***Busco el IDter dentro del vocabulario*/
+        idtermino = itoa (IDter, idtermino, 10); /*****chanchada para guardar numero como char***/
+    }
+    this->agregar_coincidencia(IDdoc, idtermino, pos);
+}
+
 int IndiceInvertido::borrar_documento(int IDdoc)
 {
     /********************FALTA*******************/
@@ -45,9 +66,76 @@ int IndiceInvertido::borrar_indice()
     this->archivo_coincidencias.eliminar_archivo(this->fileName+"Coincidencias");
 }
 
+void IndiceInvertido::crear_termino_vocabulario(ClaveX clave, char* idtermino){
+    //Agrega el nuevo termino al vocabulario con una referencia nula a la lista invertida
+    RegistroVariable reg;
+    RegistroClave regVocabulario;
+    this->archivo_terminos.agregar_registro(&reg);   //Agrego el termino al archivo de terminos
+    regVocabulario.set_clave(clave);
+    regVocabulario.agregar_campo(idtermino, sizeof(idtermino));
+    regVocabulario.agregar_campo(REF_NULA, sizeof(REF_NULA)); //Seria el numero de la lista dentro del archivo de listas invertida
+    this->vocabulario.agregar(regVocabulario);  //Agrego el nuevo termino al vocabulario
+}
+
 int IndiceInvertido::armar_listas_invertidas(int IDdoc)
 {
-    //Lo van a implementar cada indice invertido
+    int IDter, i, pos, posLista, IDterLista=-1; //ver el -1
+    RegistroVariable reg_coin, regInvertido;
+    Bloque *lista;
+    char *campo, *idtermino;
+    //Ordenamos por IDter y posicion
+    /******cuando tengamos el sort lo hacemos**/
+    //Vamos armando la lista de posiciones por IDter
+    for(i=0; i<archivo_coincidencias.get_cantidad_registros_ocupados(); i++){
+        //Saco el idtermino del registro
+        archivo_coincidencias.get_registro_ocupado(&reg_coin , i);
+        //Saco el IDter
+        reg_coin.recuperar_campo(idtermino, 0);
+        IDter = atoi(idtermino);
+        if(IDter != IDterLista){
+            //Guardo la lista en el archivo
+            if (IDterLista!=-1) listas_invertidas.sobreescribir_bloque(lista, posLista);
+            //Obtenemos la lista y su posicion en el archivo de listas invertidas
+            posLista = this->buscar_lista(IDterLista, lista);
+        }
+        reg_coin.recuperar_campo(campo, 1); /******la posicion del campo posicion*/
+        //Guardo en el registro invertido el idDoc y la posicion
+        this->crear_reg_lista_inverida(&regInvertido, campo, IDdoc);
+        lista->agregar_registro(&regInvertido);
+    }
+    return RES_OK;
+}
+
+int IndiceInvertido::buscar_lista(int IDterLista, Bloque *lista)
+{
+    //Devolvemos la posicion de la lista dentro del archivo de listas invertidas y la lista que corresponde
+    RegistroClave reg;
+    ClaveX clave;
+    char *termino, *refLista, *idtermino = itoa (IDterLista, idtermino, 10); /*****chanchada para guardar numero como char***/
+    int num;
+    /*****busco el termino en el archivo de terminos, busqueda binaria*/
+    clave.set_clave(termino);
+    reg.set_clave(clave);
+    vocabulario.buscar(reg);
+    reg.recuperar_campo(refLista,2);
+    num = atoi(refLista);   //recupero el numero
+    if(num<0){
+        //Modifico el registro para que guarde como referencia a la lista el primer bloque libre
+        num = listas_invertidas.get_primer_bloque_libre();
+        reg.limpiar_campos();
+        reg.set_clave(clave);   //Guardo el termino
+        reg.agregar_campo(idtermino, sizeof(idtermino));  //Guardo el IDtermino
+        reg.agregar_campo(refLista, sizeof(refLista));  //Guardo la referencia a la lista
+        lista = listas_invertidas.crear_bloque();
+    }else{
+        lista = listas_invertidas.obtener_bloque(num);
+    }
+    return num;
+}
+
+int IndiceInvertido::interseccion_listas_invertidas(std::string archivo)
+{
+    /***aca buscamos la interseccion de estas listas por IDdoc y guardamos las listas en un nuevo archivo de reg variables con el nombre pasado por parametro**/
 }
 
 int IndiceInvertido::armar_archivo_coincidencias(std::string doc, int IDdoc)
@@ -56,6 +144,16 @@ int IndiceInvertido::armar_archivo_coincidencias(std::string doc, int IDdoc)
 }
 
 int IndiceInvertido::buscar(char *elem_busqueda, std::string conjunto_iddoc)
+{
+    //Lo van a implementar cada indice invertido
+}
+
+int IndiceInvertido::agregar_coincidencia(int IDdoc, char* idtermino, int pos)
+{
+    //Lo van a implementar cada indice invertido
+}
+
+void IndiceInvertido::crear_reg_lista_inverida(RegistroVariable *regInvertido, char *pos, int IDdoc)
 {
     //Lo van a implementar cada indice invertido
 }
---
+++ src/CapaLogica/Indices/IndiceInvertidoPorTitulo.cpp
@@ -0,0 +1,26 @@
+#include "IndiceInvertidoPorTitulo.h"
+
+IndiceInvertidoPorTitulo::IndiceInvertidoPorTitulo()
+{
+
+}
+
+IndiceInvertidoPorTitulo::~IndiceInvertidoPorTitulo()
+{
+
+}
+
+int IndiceInvertidoPorTitulo::armar_listas_invertidas(int IDdoc)
+{
+
+}
+
+int IndiceInvertidoPorTitulo::armar_archivo_coincidencias(std::string doc, int IDdoc)
+{
+
+}
+
+int IndiceInvertidoPorTitulo::buscar(char *elem_busqueda, std::string conjunto_iddoc)
+{
+
+}
