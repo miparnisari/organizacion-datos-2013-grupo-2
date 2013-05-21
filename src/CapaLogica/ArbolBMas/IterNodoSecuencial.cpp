ec0a0d1c3a2021365e64c84adbe1c49e18992b01
maineparnisari <maineparnisari@d6609454-101f-0e72-e24d-771d2626999a>
2013-05-21 20:11:23 +0000

    Iterador de los nodos secuenciales (para las busquedas por rango del arbol)

--- src/CapaLogica/ArbolBMas/IterNodoSecuencial.cpp
+++ src/CapaLogica/ArbolBMas/IterNodoSecuencial.cpp
@@ -7,16 +7,48 @@
 
 #include "IterNodoSecuencial.h"
 
-IterNodoSecuencial::IterNodoSecuencial(NodoSecuencial nodoSec) {
-	// TODO Auto-generated constructor stub
-
+IterNodoSecuencial::IterNodoSecuencial(NodoSecuencial & nodoSec, std::string unOperador) {
+	nodo= new NodoSecuencial(nodoSec);
+	this->operador=unOperador;
 }
 
 IterNodoSecuencial::~IterNodoSecuencial() {
-	// TODO Auto-generated destructor stub
+	delete nodo;
 }
 
-int IterNodoSecuencial::get_pos_reg(std::string unOperador, ClaveX claveInicial)
+int IterNodoSecuencial::get_pos_reg(std::string unOperador,
+		ClaveX claveInicial)
 {
-	return 0;//TODO
+	if (operador == "<=" || operador == "<")
+		return 0;
+	else if	(operador==">") {
+		unsigned int i = 0;
+		for (; i < nodo->get_registros().size(); i++)
+		{
+			ClaveX* claveAlmacenada = new ClaveX(nodo->get_registros().at(i).get_clave());
+			if (claveInicial < *claveAlmacenada)
+			{
+				delete (claveAlmacenada);
+				return i;
+			}
+			delete (claveAlmacenada);
+		}
+		return i;
+	}
+	else {// caso de >=
+
+		unsigned int i = 0;
+		for (; i < nodo->get_registros().size(); i++)
+		{
+			ClaveX* claveAlmacenada = new ClaveX(nodo->get_registros().at(i).get_clave());
+
+			if (claveInicial <= *claveAlmacenada)
+			{
+				delete (claveAlmacenada);
+				return i;
+			}
+			delete (claveAlmacenada);
+		}
+		return i;
+	}
 }
