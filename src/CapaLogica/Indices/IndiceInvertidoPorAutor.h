6670282998852027d7588aa9e3125d27f78fa701
nani16.galli@gmail.com <nani16.galli@gmail.com@d6609454-101f-0e72-e24d-771d2626999a>
2013-05-22 04:52:45 +0000

    Subo modificaciones del Indice invertido por Autor, creo que ahora cumple con lo que pide el enunciado

--- src/CapaLogica/Indices/IndiceInvertidoPorAutor.h
+++ src/CapaLogica/Indices/IndiceInvertidoPorAutor.h
@@ -1,18 +1,35 @@
 #ifndef INDICEINVERTIDOPORAUTOR_H_INCLUDED
 #define INDICEINVERTIDOPORAUTOR_H_INCLUDED
 
-#include "IndiceInvertidoListaConDoc.h"
+#include "../../CapaFisica/ManejadorBloques.h"
+#include "../../CapaFisica/ManejadorRegistrosVariables.h"
+#include "../../CapaFisica/RegistroVariable.h"
+#include "../ArbolBMas/ArbolBMas.h"
+#include "../../CapaLogica/ManejoArchivos/ClaveX.h"
+#include "../../CapaLogica/ManejoArchivos/RegistroClave.h"
+#include "../../Constantes.h"
+#include "../../CapaLogica/ManejoArchivos/RegistroCancion.h"
 
-class IndiceInvertidoPorAutor : public IndiceInvertidoListaConDoc
+class IndiceInvertidoPorAutor
 {
+    private:
+        std::string ruta;
+        std::string direccion_listas;
+        ArbolBMas indice;
+        ManejadorRegistrosVariables listas;
 
     public:
         IndiceInvertidoPorAutor();
         virtual ~IndiceInvertidoPorAutor();
+        virtual int crear_indice(std::string directorioSalida);
+        virtual int abrir_indice(std::string directorioSalida);
 
-    protected:
-        std::string fileName;
-        virtual int armar_archivo_coincidencias(std::string doc, int IDdoc);
+        virtual int agregar_cancion(RegistroCancion cancion, int IDcancion);
+        virtual long buscar_autor(std::string autor, RegistroVariable &listaDeCanciones);
+        virtual int borrar_indice();
+
+    private:
+        virtual int recontruir_listas(unsigned short* ref_listas, unsigned short cant_ref, int IDcancion);
 };
 
 #endif // INDICEINVERTIDOPORAUTOR_H_INCLUDED
