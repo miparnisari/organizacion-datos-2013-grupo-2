ea80bc95b7045bbce9298b106107e67492db340b
nani16.galli@gmail.com <nani16.galli@gmail.com@d6609454-101f-0e72-e24d-771d2626999a>
2013-05-24 09:14:03 +0000

    Bueno, teoricamente la logica esta terminada, faltan los test y ordenar archivos cuando tenga el sort externo

--- src/CapaLogica/Indices/IndiceInvertido.h
+++ src/CapaLogica/Indices/IndiceInvertido.h
@@ -16,6 +16,7 @@ class IndiceInvertido
 {
     //Atributos
     protected:
+
         std::string fileName;
         std::string ruta;
         ManejadorRegistrosVariables archivo_terminos;
@@ -25,33 +26,54 @@ class IndiceInvertido
         ManejadorRegistrosVariables archivo_coincidencias;
 
     public:
+
         IndiceInvertido();
+
         virtual ~IndiceInvertido();
+
         virtual int crear_indice(std::string directorioSalida, std::string fileNamee);
+        //Crea los archivos necesarios para el indice
+
         virtual int abrir_indice(std::string directorioSalida, std::string fileNamee);
+        //Abre los archivos que necesita el indice
+
         virtual int agregar_cancion(RegistroCancion cancion, int IDcancion);
+        //Agrega los terminos de la cancion al indice junto a su posicion
+
         virtual int borrar_indice();
+        //Borra el indice junto a sus archivos
+
         virtual int buscar_frase(std::string frase, RegistroVariable &lista);
+        //Busca aquellas canciones que contienen la frase pasada por parametro y las guarda en la lista
 
-    protected:
-        virtual int armar_archivo_terminos_frase(std::string frase, RegistroVariable canciones, RegistroVariable &terminos_frase);
+    private:
+
+        virtual int armar_archivo_coincidencias(std::string letra);
+        //Guarda en un archivo de coincidencias el (IDter, pos) de cada termino en la letra
+
+        virtual int armar_listas_invertidas(int IDcancion);
+        //Guarda en la lista invertida de cada termino una lista con las posiciones de esta cancion
+
         virtual int interseccion_listas_invertidas(std::string frase, RegistroVariable &canciones);
-        virtual int buscar_doc_con_frase(RegistroVariable terminos_frase);
-        virtual int obtener_canciones_termino(const char *termino, RegistroVariable &canciones);
+        //Guarda en canciones aquellas letras que contienen todos los terminos de la frase
+
         virtual int interseccion(RegistroVariable &canciones, RegistroVariable &listaAux);
+        //Busco la interseccion de las dos listas y la guardo en la lista canciones
+
+        virtual int obtener_canciones_termino(const char *termino, RegistroVariable &canciones);
+        //Guarda en la lista de canciones todas aquellas canciones en las que aparece el termino pasado por parametro
+
+        virtual int armar_archivo_terminos_frase(std::string frase, RegistroVariable canciones, RegistroVariable &terminos_frase);
+        //Arma un archivo termporal con registros (IDcancion, pos, IDtermino) que va a servir para buscar las canciones que tienen la frase incluida
+
         virtual int buscar_cancion_en_lista(int IDcancion, RegistroVariable &canciones);
+        //Busca dentro de la la lista de canciones si se encuenta la cancion pasada por parametro
+
+        virtual int buscar_cancion_con_frase(RegistroVariable terminos_frase, RegistroVariable &lista);
+        //Busca dentro del archivo temporal que documentos tienen los terminos en el orden de la frase
+
         virtual int siguiente_termino_frase(int &pos_reg, int pos_ter_frase, int &posAnterior, int &IDcancionAnterior, RegistroVariable &terminos_frase);
-
-
-
-
-     //   virtual void crear_termino_vocabulario(ClaveX clave, char* idtermino);
-        //Esto va a depender del indice que armemos
-        virtual void crear_reg_lista_inverida(RegistroVariable *regInvertido, char *pos, int IDdoc);
-        virtual int armar_listas_invertidas(int IDdoc);
-        virtual int armar_archivo_coincidencias(std::string doc, int IDdoc);
-        virtual int buscar(char *elem_busqueda, std::string conjunto_iddoc);
-        //este metodo busca el elemento buscado y crea un archivo de registros variables con la direccion pasada por parametro
+        //Es recursiva, sirve para buscar si dentro de una cancion se encuentra los terminos en el orden de la frase, en caso que no lo este devuelve NO_PERTENECE
 
 };
 
