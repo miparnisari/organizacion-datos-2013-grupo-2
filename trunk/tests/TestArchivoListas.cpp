bb69da8d0ff1fb920e664a0d29b340af7a990437
nani16.galli@gmail.com <nani16.galli@gmail.com@d6609454-101f-0e72-e24d-771d2626999a>
2013-05-25 07:59:24 +0000

    Agrego nuevos test y modifico los del indice por autor... aun me falta un test del archivo de listas y los test del indice por frase

---
+++ trunk/tests/TestArchivoListas.cpp
@@ -0,0 +1,181 @@
+/*
+ * TestArchivoListas.cpp
+ *
+ *  Created on: 25/05/2013
+ *      Author: nadia
+ */
+
+#include "TestArchivoListas.h"
+#define DIRECCION	"TestArchivoListas.dat"
+
+TestArchivoListas::TestArchivoListas()
+{
+
+}
+
+TestArchivoListas::~TestArchivoListas()
+{
+
+}
+
+void TestArchivoListas::ejecutar()
+{
+	test_archivo_listas_crear();
+	test_archivo_listas_eliminar();
+	test_archivo_listas_agregar();
+	test_archivo_listas_devolver();
+	test_archivo_listas_reconstruir_listas();
+	test_archivo_listas_reconstruir_listas_por_frase();
+}
+
+void TestArchivoListas::test_archivo_listas_crear()
+{
+	ArchivoListas archivo;
+	ManejadorRegistrosVariables manejador;
+	//Creo el archivo
+	assert(archivo.crear("", DIRECCION) == RES_OK);
+	//Lo intento abrir
+	assert(archivo.abrir("", DIRECCION) == RES_OK);
+
+	//Veosi se guardo algo en el archivo mediante el manejador
+	assert(manejador.abrir_archivo(DIRECCION) == RES_OK);
+	assert(manejador.get_cantidad_registros_ocupados() == 0);
+
+	this->eliminar_archivos();
+	print_test_ok("test_archivo_listas_crear");
+}
+
+void TestArchivoListas::test_archivo_listas_eliminar()
+{
+	ArchivoListas archivo;
+	//Creo el archivo
+	assert(archivo.crear("", DIRECCION) == RES_OK);
+	//Lo intento abrir
+	assert(archivo.abrir("", DIRECCION) == RES_OK);
+	//Lo elimino
+	assert(archivo.eliminar("", DIRECCION) == RES_OK);
+	//Lo intento abrir
+	assert(archivo.abrir("", DIRECCION) != RES_OK);
+
+
+	this->eliminar_archivos();
+	print_test_ok("test_archivo_listas_eliminar");
+}
+
+void TestArchivoListas::test_archivo_listas_agregar()
+{
+	ArchivoListas archivo;
+	ManejadorRegistrosVariables manejador;
+	RegistroVariable lista;
+	int id=23;
+	//Creo el archivo
+	assert(archivo.crear("", DIRECCION) == RES_OK);
+	//Lo intento abrir
+	assert(archivo.abrir("", DIRECCION) == RES_OK);
+	//Agrego un elemento a la lista
+	lista.agregar_campo((char*)&id,0);
+	//Agrego la lista al archivo
+	archivo.agregar(&lista);
+	assert(archivo.get_cantidad_listas() == 1);
+
+	//Veo que se guardo en el archivo mediante el manejador
+	assert(manejador.abrir_archivo(DIRECCION) == RES_OK);
+	assert(manejador.get_cantidad_registros_ocupados() == 1);
+	assert(manejador.get_registro_ocupado(&lista,0) == RES_OK);
+	assert(lista.get_cantidad_campos() == 1);
+	lista.recuperar_campo((char*)&id,0);
+	assert(id == 23);
+
+	this->eliminar_archivos();
+	print_test_ok("test_archivo_listas_agregar");
+}
+
+void TestArchivoListas::test_archivo_listas_devolver()
+{
+	ArchivoListas archivo;
+	ManejadorRegistrosVariables manejador;
+	RegistroVariable lista;
+	int id=23;
+	//Creo el archivo
+	assert(archivo.crear("", DIRECCION) == RES_OK);
+	//Lo intento abrir
+	assert(archivo.abrir("", DIRECCION) == RES_OK);
+	//Agrego un elemento a la lista
+	lista.agregar_campo((char*)&id,0);
+	//Agrego la lista al archivo
+	archivo.agregar(&lista);
+
+	//Obtengo la lista que se guardo en el archivo
+	assert(archivo.get_cantidad_listas() == 1);
+	assert(archivo.devolver(&lista, 0) == RES_OK);
+	assert(lista.get_cantidad_campos() == 1);
+	lista.recuperar_campo((char*)&id,0);
+	assert(id == 23);
+
+	this->eliminar_archivos();
+	print_test_ok("test_archivo_listas_devolver");
+}
+
+void TestArchivoListas::test_archivo_listas_reconstruir_listas()
+{
+	ArchivoListas archivo;
+	ManejadorRegistrosVariables manejador;
+	RegistroVariable lista1, lista2;
+	int id=23;
+	unsigned short* ref_listas = new unsigned short[1]();
+	//Creo el archivo
+	assert(archivo.crear("", DIRECCION) == RES_OK);
+	//Lo intento abrir
+	assert(archivo.abrir("", DIRECCION) == RES_OK);
+	//Agrego 2 listas diferentes
+	//Agrego un elemento a la lista1
+	lista1.agregar_campo((char*)&id,0);
+	//Agrego la lista1 al archivo
+	archivo.agregar(&lista1);
+	//Agrego un elemento a la lista2
+	id=11;
+	lista1.agregar_campo((char*)&id,0);
+	//Agrego la lista2 al archivo
+	archivo.agregar(&lista1);
+
+	//Agrego a las ref_listas el valor 1, que seria la posicion de la segunda lista en el archivo
+	ref_listas[0]=1;
+	//Reconstruimos las listas del archivo, o sea que vamos a agregarle a la lista 2 el id = 9
+	assert(archivo.recontruir_listas(ref_listas, 1, 9) == RES_OK);
+	//Voy a ver que solo hayan 2 listas en el archivo
+	assert(archivo.get_cantidad_listas() == 2);
+
+	//Voy a ver que las listas tengan los id correctos
+
+	//Obtengo la lista1 que se guardo en el archivo
+	assert(archivo.devolver(&lista1, 0) == RES_OK);
+	assert(lista1.get_cantidad_campos() == 1);
+	lista1.recuperar_campo((char*)&id,0);
+	assert(id == 23);
+	//Obtengo la lista2 que se guardo en el archivo
+	assert(archivo.devolver(&lista2, 1) == RES_OK);
+	assert(lista2.get_cantidad_campos() == 2);
+	lista2.recuperar_campo((char*)&id,0);
+	assert(id == 11);
+	lista2.recuperar_campo((char*)&id,1);
+	assert(id == 9);
+
+
+	this->eliminar_archivos();
+	print_test_ok("test_archivo_listas_reconstruir_listas");
+}
+
+void TestArchivoListas::test_archivo_listas_reconstruir_listas_por_frase()
+{
+
+	this->eliminar_archivos();
+	print_test_ok("test_archivo_listas_reconstruir_listas_por_frase");
+}
+
+void TestArchivoListas::eliminar_archivos()
+{
+	//Elimina los archivos que se usan en cada archivo
+	ManejadorArchivos manejador;
+	manejador.eliminar_archivo(DIRECCION);
+}
+
--- trunk/tests/TestIndicePorAutor.h
+++ trunk/tests/TestIndicePorAutor.h
@@ -1,13 +1,13 @@
 #ifndef TESTINDICEPORAUTOR_H_INCLUDED
 #define TESTINDICEPORAUTOR_H_INCLUDED
 
-#include "CapaLogica/Indices/IndiceInvertidoPorAutor.h"
-#include "CapaLogica/Indices/ArchivoListas.h"
-#include "CapaFisica/ManejadorArchivos.h"
-#include "CapaLogica/ManejoArchivos/RegistroCancion.h"
-#include "CapaFisica/RegistroVariable.h"
-#include "CapaLogica/ManejoArchivos/RegistroClave.h"
-#include "CapaLogica/ManejoArchivos/ClaveX.h"
+#include "../src/CapaLogica/Indices/IndiceInvertidoPorAutor.h"
+#include "../src/CapaLogica/Indices/ArchivoListas.h"
+#include "../src/CapaFisica/ManejadorArchivos.h"
+#include "../src/CapaLogica/ManejoArchivos/RegistroCancion.h"
+#include "../src/CapaFisica/RegistroVariable.h"
+#include "../src/CapaLogica/ManejoArchivos/RegistroClave.h"
+#include "../src/CapaLogica/ManejoArchivos/ClaveX.h"
 #include "Test.h"
 
 class TestIndicePorAutor : public Test
@@ -22,25 +22,25 @@ class TestIndicePorAutor : public Test
 
     private:
 
-        void test_crear_indice();
+        void test_indice_autor_crear_indice();
         //Crea el indice y verificamos que se creen todos los archivos como corresponde
 
-        void test_agregar_cancion();
+        void test_indice_autor_agregar_cancion();
         //Agregamos una cancion y verificamos que se guarde correctamente en todos los archivos
 
-        void test_devolver_canciones_por_autor();
+        void test_indice_autor_devolver_canciones_por_autor();
         //Probamos que si no se agrego ninguna cancion devuelva NO_EXISTE, luego agregamos una cancion
         //y vemos si la devuelve correctamente y tambien probamos pedirle una cancion que no existe,
         //deberia devolver NO_EXISTE
 
-        void test_borrar_indice();
+        void test_indice_autor_borrar_indice();
         //Probamos que se borren todos los archivos que tenian
 
-        void test_agregar_muchas_canciones();
+        void test_indice_autor_agregar_muchas_canciones();
         //Probamos que al agregar muchas canciones en el indice las listas se creen correctamente y
         //vemos si se guarda correctamente los registros de autores como corresponde
 
-        void test_devolver_muchas_canciones_por_autor();
+        void test_indice_autor_devolver_muchas_canciones_por_autor();
         //Probamos que se devuelvan todas las canciones que correspondan a un autor con muchas canciones
         // y a otro con pocas canciones asociadas a el
 
@@ -48,7 +48,7 @@ class TestIndicePorAutor : public Test
 
         virtual void eliminar_archivos();
 
-        virtual void crear_reg_cancion(char* autor, RegistroCancion &reg);
+        virtual void crear_reg_cancion(const char* autor, RegistroCancion &reg);
 };
 
 #endif // TESTINDICEPORAUTOR_H_INCLUDED
