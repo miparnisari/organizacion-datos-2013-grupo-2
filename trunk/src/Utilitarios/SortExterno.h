dacfdfc556d9fd54e9aa5a8c9e76a4532ed87fc9
juanfedericofuld@hotmail.com <juanfedericofuld@hotmail.com@d6609454-101f-0e72-e24d-771d2626999a>
2013-05-25 17:55:57 +0000

    ahora si estan

---
+++ trunk/src/Utilitarios/SortExterno.h
@@ -0,0 +1,54 @@
+/*
+ * SortExterno.h
+ *
+ *  Created on: May 16, 2013
+ *      Author: juan
+ */
+
+#ifndef SORTEXTERNO_H_
+#define SORTEXTERNO_H_
+
+using namespace std;
+#include <string>
+#include<vector>
+
+
+#include "../Utilitarios/Heap.h"
+#include "../CapaFisica/ManejadorRegistrosVariables.h"
+
+
+class SortExterno {
+public:
+	SortExterno(std::string archAOrdenar);
+	virtual ~SortExterno();
+
+	//ordenar_archivo(string nombreArchivo);
+	/*
+	 * Sobreescribe el archivo original
+	 * */
+
+	/*SOLO PARA TESTS*****************************************/
+
+	std::vector<string> _getVector()
+	{
+		return archivosTemporalesAFusionar;
+	}
+
+	void _generar_runs();
+	/*
+	 * Utiliza replacement selection
+	 */
+
+	void _merge();
+	/*
+	 * Fusiona todos los runs generados
+	 * */
+
+private:
+
+	unsigned cantDeRuns;
+	std::string archAOrdenar;
+	std::vector<string> archivosTemporalesAFusionar;
+};
+
+#endif /* SORTEXTERNO_H_ */
