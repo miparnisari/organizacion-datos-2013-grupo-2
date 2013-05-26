2b79c35400d3b4fde3025a007fd75ffc837e1d66
juanfedericofuld@hotmail.com <juanfedericofuld@hotmail.com@d6609454-101f-0e72-e24d-771d2626999a>
2013-05-26 16:14:02 +0000

    algunos cambios menores

--- trunk/src/Constantes.h
+++ trunk/src/Constantes.h
@@ -89,7 +89,7 @@ const int RES_BALANCEO				= -5;
 
 const unsigned int TAM_MAX_BUFFER_ORDENAMIENTO	=1024; //en bytes
 //dado que los campos por los que se va a ordenar son secuencias de char...
-const char	CLAVE_TOPE								= char(256);
+const char	CLAVE_TOPE								= char(255);
 
 /*
  *Manejo del IndiceInvertido
