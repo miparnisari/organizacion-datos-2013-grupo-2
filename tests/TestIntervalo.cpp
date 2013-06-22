5a9a91d34bae5a83da9641c6d3b482e62ea510e4
maineparnisari <maineparnisari@d6609454-101f-0e72-e24d-771d2626999a>
2013-06-22 16:50:49 +0000

    modifico los tests de intervalo para que trabajen con 32 bits de precision

--- tests/TestIntervalo.cpp
+++ tests/TestIntervalo.cpp
@@ -9,19 +9,17 @@
 #include "../lib/gtest-1.6.0/include/gtest/gtest.h"
 
 /*
- *
- * NOTA: ESTOS TESTS SE PROBARON CON PRECISION = 7.
+ * NOTA: ESTOS TESTS SE PROBARON CON PRECISION = 32.
  * CON OTRA PRECISION NO VAN A FUNCIONAR.
  *
  */
 
-// To use a test fixture, derive a class from testing::Test.
 class TestIntervalo : public testing::Test {
  protected:
-	// Declares the variables your tests want to use.
 	Intervalo intervalo;
+
   virtual void SetUp() {
-	  unsigned long long rango_inicial = 128;
+	  unsigned long long rango_inicial = 4294967296;
 	  ASSERT_EQ(intervalo.get_rango(), rango_inicial );
   }
 
@@ -52,8 +50,8 @@ TEST_F(TestIntervalo,Crear)
 TEST_F(TestIntervalo, Normalizar_con_overflow)
 {
 
-	intervalo.set_piso("0000000");
-	intervalo.set_techo("0111111");
+	intervalo.set_piso("00000000000000000000000000000000");
+	intervalo.set_techo("01111111111111111111111111111111");
 
 	// Se produce un overflow
 	Byte cOverflow,cUnderflow;
@@ -61,16 +59,16 @@ TEST_F(TestIntervalo, Normalizar_con_overflow)
 	ASSERT_EQ(resultado.size(),1);
 	ASSERT_EQ(resultado[0],0);
 
-	std::bitset<PRECISION> piso_despues_de_normalizar (std::string("0000000"));
-	std::bitset<PRECISION> techo_despues_de_normalizar (std::string("1111111"));
+	std::bitset<PRECISION> piso_despues_de_normalizar (std::string("00000000000000000000000000000000"));
+	std::bitset<PRECISION> techo_despues_de_normalizar (std::string("11111111111111111111111111111111"));
 	ASSERT_EQ(intervalo.get_piso(), piso_despues_de_normalizar);
 	ASSERT_EQ(intervalo.get_techo(), techo_despues_de_normalizar);
 }
 
 TEST_F(TestIntervalo, Normalizar_con_overflow_y_underflow)
 {
-	intervalo.set_piso("1001010");
-	intervalo.set_techo("1010111");
+	intervalo.set_piso("10010100000000000000000000000000");
+	intervalo.set_techo("10101111111111111111111111111111");
 
 	// Se produce un overflow de "10"
 	Byte cOverflow,cUnderflow;
@@ -79,16 +77,16 @@ TEST_F(TestIntervalo, Normalizar_con_overflow_y_underflow)
 	ASSERT_EQ(resultado[0],1);
 	ASSERT_EQ(resultado[1],0);
 
-	std::bitset<PRECISION> piso_despues_de_normalizar (std::string("0010000"));
-	std::bitset<PRECISION> techo_despues_de_normalizar (std::string("1111111"));
+	std::bitset<PRECISION> piso_despues_de_normalizar (std::string("00100000000000000000000000000000"));
+	std::bitset<PRECISION> techo_despues_de_normalizar (std::string("11111111111111111111111111111111"));
 	ASSERT_EQ(intervalo.get_piso(), piso_despues_de_normalizar);
 	ASSERT_EQ(intervalo.get_techo(), techo_despues_de_normalizar);
 }
 
 TEST_F(TestIntervalo, Normalizar_con_underflow)
 {
-	intervalo.set_piso("0110010");
-	intervalo.set_techo("1000011");
+	intervalo.set_piso("01100100000000000000000000000000");
+	intervalo.set_techo("10000111111111111111111111111111");
 
 	ASSERT_EQ(intervalo.hay_underflow(),true);
 
@@ -97,8 +95,9 @@ TEST_F(TestIntervalo, Normalizar_con_underflow)
 	ASSERT_EQ(resultado.size(),0);
 
 	// Quedan iguales porque no se normalizo nada
-	std::bitset<PRECISION> piso_despues_de_normalizar (std::string("0001000"));
-	std::bitset<PRECISION> techo_despues_de_normalizar (std::string("1001111"));
+
+	std::bitset<PRECISION> piso_despues_de_normalizar (std::string("00010000000000000000000000000000"));
+	std::bitset<PRECISION> techo_despues_de_normalizar (std::string("10011111111111111111111111111111"));
 	ASSERT_EQ(intervalo.get_piso(), piso_despues_de_normalizar);
 	ASSERT_EQ(intervalo.get_techo(), techo_despues_de_normalizar);
 }
